53362c837ee34d05369d1f173dc584adf4e99985
maranzan <arsalanramzan811@gmial.com>
2023-06-27 19:38:48 +0500

    Working on marquee page

--- app/pages/marquee/page.tsx
+++ app/pages/marquee/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "@/app/component/Navbar";
 import Footer from "@/app/component/footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,6 +8,10 @@ import { Data } from "./data";
 import { useRouter } from "next/navigation";
 function Marquee() {
   const router = useRouter();
+  const [sliderValue, setSliderValue] = useState("");
+  const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+  };
   return (
     <div>
       <Navbar />
@@ -21,30 +25,104 @@ function Marquee() {
 
       <div className=" mt-32  flex md:container mx-auto ">
         <div className="w-[25%] ">
-          <h1 className="font-vollkorn text-xl ">City</h1>
-          <input
-            type="text"
-            placeholder="city"
-            className="py-3 border-r-gray-200 mt-6 border-[1px] outline-none rounded-md px-3 w-full "
-          />
+          <div>
+            <h1 className="font-vollkorn text-xl ">Booking Details</h1>
+            <input
+              type="text"
+              placeholder="Check In  Check Out"
+              className="py-3 border-r-gray-200 mt-6 border-[1px] outline-none rounded-md px-3 w-full "
+            />
+            <input
+              type="text"
+              placeholder="Guest 1"
+              className="py-3 border-r-gray-200 mt-6 border-[1px] outline-none rounded-md px-3 w-full "
+            />
+          </div>
+          <div>
+            <h1 className="font-vollkorn text-xl my-6">Branch</h1>
+            <input
+              type="text"
+              value={"Faisalabad"}
+              className="py-3 border-r-gray-200 mt-6 border-[1px] outline-none rounded-md px-3 w-full "
+            />
+          </div>
+          <div>
+            <h1 className="font-vollkorn text-xl my-6">Price</h1>
+            <input
+              type="range"
+              min="150"
+              max="500"
+              step="10"
+              value={sliderValue}
+              onChange={handleSliderChange}
+              className="w-full"
+            />
+            <p>Slider Value: {sliderValue}</p>
+          </div>
+          <div>
+            <h1 className="font-vollkorn text-xl my-7">Included Services</h1>
+            <ul className="text-textColor font-semibold font-vollkorn overflow-y-auto scrollbar-thumb-blue-500 scrollbar-track-blue-200 h-44">
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Towels</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Microwave</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Parking</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Private Balcony</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Air Conditioner</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Widescreen TV</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Coffee Maker</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Hair Dryer</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Breakfast</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Mini Bar</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>WiFi</li>
+            </ul>
+          </div>
+          <div>
+            <h1 className="ont-vollkorn text-xl my-9">
+            Additional Services
+            </h1>
+            <ul>
+
+            <li><input type="checkbox"/><span className="checkmark mr-2"></span>Excursions</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Airport Pickup</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Massage</li>
+              <li><input type="checkbox"/><span className="checkmark mr-2"></span>Jacuzzi</li>
+              </ul>
+            
+
+
+
+
+
+          </div>
         </div>
         <div className="w-[75%]">
           {Data.map((item) => (
-            <div key={item.id} className="mb-10 mx-5 " onClick={() => router.push("/pages/marqueedetail")}>
+            <div
+              key={item.id}
+              className="mb-10 mx-5 "
+              onClick={() => router.push("/pages/marqueedetail")}
+            >
               <div className="md:container mx-auto flex flex-col md:flex-row border-gray-200 border-[1px] rounded-lg  ">
                 <div className="md:w-[40%]  ">
-                  <img src={item.src} className="md:rounded-r-none rounded-lg " alt="" />
+                  <img
+                    src={item.src}
+                    className="md:rounded-r-none rounded-lg "
+                    alt=""
+                  />
                 </div>
                 <div className="pt-6 px-6 md:w-[40%] ">
                   <h1 className="font-vollkorn text-2xl">{item.name}</h1>
 
                   <p className="font-roboto text-textColor mt-4">{item.desc}</p>
-                  <p className="font-roboto text-textColor mt-4">{item.desc}</p>
+                  {/* <p className="font-roboto text-textColor mt-4">{item.desc}</p> */}
                   <p className="font-roboto text-textColor mt-6">Jaranwala</p>
                 </div>
                 <div className="md:w-[20%] border-l-[1px] flex flex-col justify-center mt-5 md:mt-0 ">
-                  <p className="text-center text-2xl font-roboto font-bold  ">{item.price}</p>
-                  <p className="text-center mt-3 mb-6 font-vollkorn">Full day</p>
+                  <p className="text-center text-2xl font-roboto font-bold  text-textColor">
+                    {item.price}
+                  </p>
+                  <p className="text-center mt-3 mb-6 font-vollkorn text-textColor">
+                    Per Night
+                  </p>
                   <div className="flex justify-center ">
                     <button className="text-primaryColor border-[1px] hover:text-white  hover:bg-primaryColor border-gray-200  text-center items-center justify-around flex mb-6 md:mb-0  py-2 px-4 rounded-full">
                       <p className="font-roboto">More detail</p>
